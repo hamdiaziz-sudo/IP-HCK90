5cc4f6b954b60e2efd39efcacaa66dc644585a9b
hamdiaziz <hamdibdg@gmail.com>
2025-12-09 22:02:09 +0700

    try to improve search by mood

--- server/routes/search.js
+++ server/routes/search.js
@@ -6,6 +6,8 @@ const upload = require("../helpers/multer");
 
 router.use(authentication);
 
+router.post("/mood", SearchController.recommendByMood);
+
 router.post("/text", SearchController.searchByText);
 
 router.post("/voice", upload.single("audio"), SearchController.searchByVoice);
